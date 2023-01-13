1067662c92c8f93cd7cda2734a9cc08883cd3b04
tamoroso <amorosothomas@orange.fr>
2023-01-13 11:40:08 +0100

    adding basic library and demo setup

---
+++ packages/lib/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "react_modal_oc",
+      formats: ["es", "umd"],
+      fileName: (format) => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
